1eb2ee18f853778e17ac289408ade6583e3b875e
HAIYUE YUAN <haiyue.yuan@gmail.com>
2021-11-09 12:08:59 +0900

    delete recipe endpoint

--- main.go
+++ main.go
@@ -72,10 +72,32 @@ func UpdateRecipeHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, recipe)
 }
 
+func DeleteRecipeHandler(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found"})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted."})
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	router.Run()
 }
